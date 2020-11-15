7a838b264fed82d9d67a5786eac0483f2f3cb7bf
abe-kiyotaka <abe.kiyotaka.mp@gmail.com>
2020-11-15 19:25:08 +0900

    :sparkles: キーボードイベントを追加

--- game.js
+++ game.js
@@ -31,18 +31,28 @@ app.renderer.resize(1200, 800);
 //app.renderer.autoResize = true;
 //app.renderer.resize(window.innerWidth, window.innerHeight);
 
+// loader設定
 PIXI.loader
   .add("images/renge.png")
   .add("images/inaka.jpg")
   .load(setup);	
 
+// グローバル変数
 let renge
 let inaka
-//Spriteにテクスチャキャッシュから読み込む
+let message
+
+// onLoad処理
 function setup() {
   loadImages()
-  makeText()
+  message = makeText()
   app.ticker.add(delta => gameLoop(delta));
+
+  let right = keyboard("ArrowRight");
+  right.press = () => {
+    message.setText("右やじるしをおされると、左に移動しちゃうのん！")
+    renge.x 
+  }
 }
 
 //右に移動
@@ -85,5 +95,53 @@ function makeText(){
   let message = new Text("わたしはれんげ、みぎにいくのん！");
   message.position.set(100, 680);
   app.stage.addChild(message);
+
+  return message
+}
+
+function keyboard(value) {
+  let key = {};
+  key.value = value;
+  key.isDown = false;
+  key.isUp = true;
+  key.press = undefined;
+  key.release = undefined;
+  //The `downHandler`
+  key.downHandler = event => {
+    if (event.key === key.value) {
+      if (key.isUp && key.press) key.press();
+      key.isDown = true;
+      key.isUp = false;
+      event.preventDefault();
+    }
+  };
+
+  //The `upHandler`
+  key.upHandler = event => {
+    if (event.key === key.value) {
+      if (key.isDown && key.release) key.release();
+      key.isDown = false;
+      key.isUp = true;
+      event.preventDefault();
+    }
+  };
+
+  //Attach event listeners
+  const downListener = key.downHandler.bind(key);
+  const upListener = key.upHandler.bind(key);
   
+  window.addEventListener(
+    "keydown", downListener, false
+  );
+  window.addEventListener(
+    "keyup", upListener, false
+  );
+  
+  // Detach event listeners
+  key.unsubscribe = () => {
+    window.removeEventListener("keydown", downListener);
+    window.removeEventListener("keyup", upListener);
+  };
+  
+  return key;
 }
